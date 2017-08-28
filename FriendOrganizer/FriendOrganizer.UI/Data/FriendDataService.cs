52f31721783976ba6ffa24e3cc578a5706dcf95e
Wisnu Anggoro <wisnu@anggoro.net>
2017-08-28 14:26:08 +0700

    Basic UI
    Building the basic UI layer

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from real database
+            yield return new Friend { FirstName = "Arthur", LastName = "Black" };
+            yield return new Friend { FirstName = "Holly", LastName = "Newman" };
+            yield return new Friend { FirstName = "Remy", LastName = "Villarreal" };
+            yield return new Friend { FirstName = "Kassandra", LastName = "Gardner" };
+        }
+    }
+}
